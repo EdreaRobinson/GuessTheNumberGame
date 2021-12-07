67dfe6a6fc8a49675bd86894e127e93c854c4630
EdreaRobinson <edrea1979@gmail.com>
2021-12-07 12:19:32 -0600

    Greet player and get player name

--- game.py
+++ game.py
@@ -2,4 +2,16 @@
 
 # Put your code here
 
-print('hi')
+# greet player
+name = input("Hey! What is your name? ")
+
+# get player name
+print(f"{name}, I'm thinking of a number between 1 and 100")
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
