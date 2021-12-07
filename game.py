044a28aae94a5deaa4bf9bda064d2f0f5781ab9e
EdreaRobinson <edrea1979@gmail.com>
2021-12-07 12:25:07 -0600

    Random number generated

--- game.py
+++ game.py
@@ -1,13 +1,17 @@
 """A number-guessing game."""
 
 # Put your code here
+import random
 
 # greet player
 name = input("Hey! What is your name? ")
 
 # get player name
 print(f"{name}, I'm thinking of a number between 1 and 100")
+
 # choose random number between 1 and 100
+number = random.randint(1, 100)
+print (number)
 # repeat forever:
 #     get guess
 #     if guess is incorrect:
